8c01160a9dd3f2e061249a55ae68561fb4128143
yanliu1111 <liuyan0504@gmail.com>
2023-09-14 23:03:29 -0600

    feat: delete categories

--- admin/categories.php
+++ admin/categories.php
@@ -39,10 +39,7 @@ if (isset($_POST['submit'])){
                         </form>
                     </div> <!-- add category form -->
                     <div class="col-xs-6">
-                        <?php
-                    $query = "SELECT * FROM categories";
-                    $select_categories = mysqli_query($connection, $query); 
-                    ?>
+
                         <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
@@ -51,16 +48,29 @@ if (isset($_POST['submit'])){
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php
-                    while ($row=mysqli_fetch_array ( $select_categories)){
-                    $cat_id = $row['cat_id'];
-                    $cat_title = $row['cat_title'];
-                    echo "<tr>";
-                    echo "<td>{$cat_id}</td>";
-                    echo "<td>{$cat_title}</td>";
+                                <?php // find all categories query
+                                 $query = "SELECT * FROM categories";
+                                 $select_categories = mysqli_query($connection, $query); 
+                                 
+                                while ($row=mysqli_fetch_array ( $select_categories)){
+                                $cat_id = $row['cat_id'];
+                                $cat_title = $row['cat_title'];
+                                echo "<tr>";
+                                echo "<td>{$cat_id}</td>";
+                                echo "<td>{$cat_title}</td>";
+                                echo "<td> <a href = 'categories.php?delete={$cat_id}'>
+                                Delete</a></td>";
+                        
                     echo "</tr>";
                     } ?>
-
+                                <?php // delete query
+                        if (isset($_GET['delete'])){
+                            $the_cat_id = $_GET['delete'];
+                            $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+                            $delete_query = mysqli_query($connection, $query);
+                            header("Location: categories.php"); // refresh page
+                        }
+                        ?>
                             </tbody>
                         </table>
                     </div>
